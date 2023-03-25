ee0e73c7ab07c04783a778369b5f12afa2a27488
Kate Kashko <katekashkobiz@gmail.com>
2023-03-25 10:41:19 +0300

    Initial Commit

---
+++ HW Lesson 2.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW Lesson 2.4
+//
+//  Created by Kate Kashko on 25.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
